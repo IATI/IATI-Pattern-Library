627bed63e8213263dc5de5a2f11682cbd6ed4ab4
Alex Miller <alex.k.miller@gmail.com>
2018-06-21 13:37:05 -0400

    Add commit and origin-pages upstream

--- .travis/subtree.sh
+++ .travis/subtree.sh
@@ -5,9 +5,16 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
+commit_website_files() {
+  git add .
+  git commit --message "Travis build $TRAVIS_BUILD_NUMBER"
+}
+
 upload_files() {
-  git subtree push --prefix converted-html origin gh-pages
+  git remote add origin-pages https://${GH_TOKEN}@github.com/IATI/IATI-Pattern-Library.git > /dev/null 2>&1
+  git subtree push --prefix converted-html origin-pages gh-pages
 }
 
 setup_git
+commit_website_files
 upload_files
