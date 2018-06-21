0bd4770a0babd9d9ad0074867408be8d4fdf52bc
Alex Miller <alex.k.miller@gmail.com>
2018-06-21 12:04:52 -0400

    Revert "Manual serve, copy to docs"
    This reverts commit ea97f1904a8bd34e0ed0bb335353084dd167a393.

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -33,51 +33,9 @@ function globals () {
         '#navigation-secondary',
         'navigation-secondary--active'
     );
-
-    // Load EventBrite if the window is larger than 500px, which is our $b-vp breakpoint
-    if ($(window).width() > 500) {
-        var targetID = 'eventbrite-widget-container';
-        $.getScript("https://www.eventbrite.co.uk/static/widgets/eb_widgets.js", function( data, textStatus, jqxhr ) {
-            var exampleCallback = function() {
-                console.log('Registration complete!');
-            };
-            window.EBWidgets.createWidget({
-                // Required
-                widgetType: 'checkout',
-                eventId: '33395177876',
-                iframeContainerId: targetID,
-                // Optional
-                iframeContainerHeight: 285,  // Widget height in pixels
-                onOrderComplete: exampleCallback  // Method called when an order has successfully completed
-            });
-        });
-    }
-    // Modify behaviour of Register button depending on window width
-    $('#button-register').click(function(event) {
-        if ($(window).width() > 500) {
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: $('#'+targetID).offset().top
-            }, 300);
-        }
-    });
-
-    $('.js-move').click(function(event) {
-        event.preventDefault();
-        var target = $(this).attr('data-target');
-        $('#' + target).scrollView();
-    });
 }
 
 $(function run () {
     console.log('ᕕ( ᐛ )ᕗ Running...');
     globals();
 });
-
-$.fn.scrollView = function () {
-  return this.each(function () {
-    $('html, body').animate({
-      scrollTop: $(this).offset().top
-    }, 600);
-  });
-}
