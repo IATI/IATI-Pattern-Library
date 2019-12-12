bc68ec482233620a9e67ddca695ca196886fdfe4
Russell Kirkland <russell@mashandgravy.co.uk>
2019-12-12 10:57:59 +0000

    Focus search input when triggered from utility nav

--- assets/js/utils/searchTrigger.js
+++ assets/js/utils/searchTrigger.js
@@ -1,9 +1,10 @@
 import $ from 'jquery';
 
-export default function mNav (trigger, target, targetActive, triggerClose) {
+export default function mNav (trigger, target, targetActive, triggerClose, field = '[name=query]') {
     $(trigger).on('click', function(e) {
 		e.preventDefault();
 		$(target).addClass(targetActive);
+        $(target).find(field).focus();
 	});
     $(triggerClose).on('click', function(e) {
 		e.preventDefault();
